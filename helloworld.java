27fd1395bcc9905ef543acc0f16bf73802b7ce0f
phanidanuluri <37099936+phanidanuluri@users.noreply.github.com>
2020-11-30 13:40:47 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld
 {
   public static void main(String args[])
   {
-    System.out.println("ronaldo-------------");
+    System.out.println("durga matha-------------");
   }
 
 }
